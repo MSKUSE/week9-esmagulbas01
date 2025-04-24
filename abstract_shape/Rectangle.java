881be83cd1b16af27af7bf3407fa747244f69153
esmagulbas01 <esmagulbas@posta.mu.edu.tr>
2025-04-24 13:15:12 +0300

    week9

---
+++ abstract_shape/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle extends Shape {
+    private int sideA=0,sideB=0;
+
+    public Rectangle(){}
+
+    public Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*(sideA+sideB);
+    }
+
+    @Override
+    public double area() {
+        return sideA * sideB;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() + "Rectangle{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                '}';
+    }
+}
+
+
---
+++ abstract_shape/Shape.java
@@ -0,0 +1,16 @@
+public abstract class Shape {
+    private String colot = "white";
+
+    public Shape(){
+
+    }
+    public Shape(String color){
+        this.color = color;
+
+    }
+    public abstract double perimeter(){
+
+    }
+    public  abstract double area()
+
+}
